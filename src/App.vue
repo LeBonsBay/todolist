d5e320ae9aab86d0ddb6d6e827ce5085aa5af5a8
LeBonsBay <armin.keric0001@gmail.com>
2025-07-12 14:21:43 +0200

    created project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import Todolist from "@/components/todolist.vue";
+</script>
+
+<template>
+
+ <todolist></todolist>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/todolist.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+import {ref} from 'vue'
+
+let y = ref(false)
+
+const tasks = [
+  {title: 'Finish Project'},
+  {title: 'Finish Project'},
+  {title: 'Finish Project'},
+]
+function changeCondition(){
+  y = ref(true)
+
+}
+//@ToDo
+//look how a v-if actually behaves and wors & look into v-model
+//afterwards, implement drag and drop function
+
+</script>
+
+<template>
+  <label for="myCheckbox" v-for="(task,index) in tasks">
+    <input type="checkbox">
+    {{task.title}}
+    <br>
+  </label>
+
+
+</template>
+
+<style scoped>
+</style>
