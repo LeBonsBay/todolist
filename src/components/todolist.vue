31dcb071998a19fedac78ca519264cec8afbf047
LeBonsBay <armin.keric0001@gmail.com>
2025-07-17 14:32:55 +0200

    added function for adding elements to array with push and implemented delete sorta

--- src/components/todolist.vue
+++ src/components/todolist.vue
@@ -1,33 +1,44 @@
 <script setup>
 
-import {ref} from 'vue'
+import {ref, vModelCheckbox} from 'vue'
 
 let y = ref(false)
+let input = ref('')
+let count = ref(0)
+const tasks = ref([])
 
-const tasks = [
-  {title: 'Finish Project'},
-  {title: 'Finish Project'},
-  {title: 'Finish Project'},
-]
-function changeCondition(){
-  y = ref(true)
 
+function addElement() {
+  tasks.value.push({title: input.value})
+  input.value = ''
 }
-//@ToDo
-//look how a v-if actually behaves and wors & look into v-model
-//afterwards, implement drag and drop function
 
+function deleteAfterFinish(index, event) {
+  const isChecked = event.target.checked
+
+  if (isChecked) {
+    tasks.value[index].title = ''
+    tasks.value.splice(index, 1)
+  }
+}
+
+//@ToDo
+//look how a v-if actually behaves and works & look into v-model computing etc..
+//afterwards, implement drag and drop function
+//after the drag and drop make plus for adding entries, deleting etc...
 </script>
 
 <template>
-  <label for="myCheckbox" v-for="(task,index) in tasks">
-    <input type="checkbox">
-    {{task.title}}
+  <form @submit.prevent="addElement"> <!--prevents to submit the form probably without text -->
+    <label>Add Task:</label>
+    <input v-model="input" placeholder="Bitte eingeben" class="border border-full ml-1 mt-1">
+  </form>
+  <br>
+  <label for="myCheckbox" v-for="(task,index) in tasks" :key="index">
+    <input type="checkbox" class="" draggable="true" @change="" v-model="tasks[index].done">
+    {{ task.title }}
     <br>
   </label>
-
-
 </template>
-
 <style scoped>
 </style>
