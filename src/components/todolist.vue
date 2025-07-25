4e3222a21c875199fed57aca426cf7af36d6e596
LeBonsBay <sasuek94@gmail.com>
2025-07-25 03:11:47 +0200

    implemented edit function for list entries

--- src/components/todolist.vue
+++ src/components/todolist.vue
@@ -1,8 +1,9 @@
 <script setup>
 
-import {ref, } from 'vue'
+import {ref,} from 'vue'
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import * as byPrefixAndName from "@fortawesome/free-solid-svg-icons";
+
 
 let y = ref(false)
 let input = ref('')
@@ -11,12 +12,16 @@ const tasks = ref([])
 let menuVisible = ref(false)
 let menuVisible1 = ref(false)
 let isDelete = ref(true)
+let editText = ref(null)
 
 function addElement() {
 
 
-  tasks.value.push({title: input.value, done: false})
-  input.value = ''
+  if(input.value !== ''){
+    tasks.value.push({title: input.value, done: false})
+    input.value = ''
+  }
+
 }
 
 function deleteTask(index) {
@@ -46,8 +51,22 @@ function deleteTask(index) {
       />
       <span :class="['flex-1', task.done ? 'text-gray-400 line-through' : 'text-black']">
         {{ task.title }}
+
               <FontAwesomeIcon :icon="['fas', 'trash']" class="text-red-500 cursor-pointer hover:text-red-700"
                                @click="deleteTask(index)"/>
+        <FontAwesomeIcon :icon="['fas', 'pen']" class="text-red-500 cursor-pointer hover:text-red-700"
+                         @click="editText = index"/>
+      </span>
+      <span v-if="editText === index"
+            class="border-b border-gray-400 focus:outline-none focus:border-blue-500 bg-transparent text-black w-full">
+        <input v-model="tasks[index].title" @keyup.enter="editText = null"></input>
+
+        <!--
+         1. Es wird auf Stift geklickt -> die null Variable speichert sich die momentane Stelle (index)
+         2. v-if wird genutzt falls der Fall auftritt dass die CSS angezeigt wird und dann das input feld durch <input/> erscheint
+         3. v-model bindet sich mit der tasks, AN der Stelle index PUNKT title. D.h. es wird genau das Element dynamisch ersetzt durch das neu geschriebene. Also wird bereits schon beim Schreiben das geschrieben ersetzt.
+         4. Durch @keyup.enter="editText=null" setzt du die Bedingungsvariable wieder auf den Ursprung zurueck und somit geht's aus dem span tag raus und das Editieren ist abgeschlossen
+         -->
       </span>
 
     </div>
@@ -79,7 +98,9 @@ function deleteTask(index) {
             placeholder="New Task..."
             class="border border-gray-300 rounded px-2 py-1"
         />
-        <button class="bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600" v-if="input.trim() !== ''">Hinzufügen</button>
+        <button class="bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600" v-if="input.trim() !== '' ">
+          Hinzufügen
+        </button>
       </form>
     </div>
   </div>
